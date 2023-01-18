e68d6be8ffe4f54dc4cd6e8ba65b075ad19a337a
melohan <melodie.ohan@cpnv.ch>
2023-01-18 23:34:43 +0100

    Create of a component for each section of the portfolio

---
+++ custom-portfolio/src/components/Experiences.vue
@@ -0,0 +1,14 @@
+<script setup>
+defineProps({
+  title: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <div id="app">
+     <h1>{{ title }}</h1>
+  </div>
+</template>
--- custom-portfolio/src/components/Home.vue
+++ custom-portfolio/src/components/Home.vue
@@ -2,11 +2,24 @@
 import { defineComponent } from 'vue'
 import homeData from '../assets/data/home.json'
 
+// Props
+// -------------------------------------------
+defineProps({
+  title: {
+    type: String,
+    required: true
+  }
+})
+
+// Load data for this page
+// -------------------------------------------
 const homeContent = homeData;
+
 </script>
 
 <template>
   <div id="app">
+    <h1>{{ title }}</h1>
      <div v-for="(value, key) in homeData" :key="key">
         {{ key }} : {{ value }}
     </div>
