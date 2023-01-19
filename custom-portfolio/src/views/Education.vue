f6121d15815fc1be35ca7f93613bb6d33d9f89a2
melohan <melodie.ohan@cpnv.ch>
2023-01-19 13:48:48 +0100

    Remove div#app

--- custom-portfolio/src/views/Education.vue
+++ custom-portfolio/src/views/Education.vue
@@ -8,9 +8,7 @@ defineProps({
 </script>
 
 <template>
-  <div id="app">
-    <section class="page-section bg-section p-2">
-      <h1>{{ title }}</h1>
-    </section>
-  </div>
+  <section class="page-section bg-section p-2">
+    <h1>{{ title }}</h1>
+  </section>
 </template>
