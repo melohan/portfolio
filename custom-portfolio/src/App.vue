2ed3edad26221343059cd883253a00d18e73df10
melohan <melodie.ohan@cpnv.ch>
2023-01-29 21:36:59 +0100

    Implement clean horizontal scroll

--- custom-portfolio/src/App.vue
+++ custom-portfolio/src/App.vue
@@ -5,71 +5,47 @@ import Resume from './views/Resume.vue'
 import Skills from './views/Skills.vue'
 import Realizations from './views/Realizations.vue'
 
+import {ref, shallowRef, onMounted, onBeforeUnmount} from 'vue'
+
 import {gsap} from "gsap";
 
 
-/**
- * This function implement the horizontal scroll between sections
- * @param {*} containerElement  expect an unique container
- * @param {*} sectionsElements  expect a class repeted and container in ContainerElement
- * @notes This code implement https://codepen.io/AkhilRaja/pen/PopdVMe
- */
-function horizontalScroll(containerElement, sectionsElements) {
+onMounted(() => {
+  let sections = gsap.utils.toArray(".panel");
+  let container = document.querySelector("#main-container")
 
-  let sections = gsap.utils.toArray(sectionsElements);
-  let containerScrollWidth = document.querySelector(containerElement).scrollWidth;
-
-  let horizontalScroll = gsap.timeline({
+  gsap.to(sections, {
+    xPercent: -100 * (sections.length - 1),
+    ease: "none",
     scrollTrigger: {
-      trigger: containerElement,
-      // The animation will be pinned to the scroll position
+      trigger: container,
       pin: true,
-      start: "0% 0%",
-      // The animation will continue until the end of the container element is reached
-      end: "+=" + (containerScrollWidth - window.innerWidth),
-      scrub: 0,
-      // Snap to a specific point in the scroll position (the section)
-      snap: {
-        // the animation will jump to specific points as the user scrolls
-        snapTo: 1 / (sections.length - 1),
-        duration: {min: 0.2, max: 0.3},
-        delay: 0,
-        // Start and end the animation slowly
-        ease: "sine.inOut"
-      }
+      scrub: 1,
+      snap: 1 / (sections.length - 1),
+      // base vertical scrolling on how wide the container is so it feels more natural.
+      end: "+=3500",
     }
   });
-  horizontalScroll.to(sections,
-      {
-        xPercent: -100 * (sections.length - 1),
-        ease: "none"
-      });
-}
+})
 
-var mainContainer = "#main-container"
-var sections = ".main-section"
-
-window.onload = () => {
-  horizontalScroll(mainContainer, sections);
-};
 </script>
 
 <template>
   <div id="app">
     <div id="main-container">
-      <section class="main-section bg-dark">
+      <section class="panel bg-dark">
         <Home title="Welcome"/>
       </section>
-      <section class="main-section bg-light">
+      <section class="panel bg-light">
         <About title="About me"/>
       </section>
-      <section class="main-section bg-dark">
+      <section class="panel bg-dark">
         <Resume title="My resume"/>
       </section>
-      <section class="main-section bg-light">
+      <section class="panel bg-light">
         <Skills title="Skills"/>
       </section>
-      <section class="main-section bg-dark">
+      <section class="panel bg-dark">
         <Realizations title="Realizations"/>
       </section>
     </div>
@@ -103,16 +79,19 @@ body {
 body {
   overflow: hidden; /* Hide scrollbars */
   overflow-x: hidden; /* Hide horizontal scrollbar */
+  overscroll-behavior: none;
+  height: 100vh;
 }
 
 #main-container {
+  overscroll-behavior: none;
   height: 100vh;
   width: 600vw; /* 100vw unit per sections */
   display: flex;
   flex-wrap: nowrap;
 }
 
-.main-section {
+.panel {
   width: 100vw;
   height: 100vh;
   display: flex;
